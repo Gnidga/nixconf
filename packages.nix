2c38a6f569d0713564ca3db0eeccf3202341ab40
Gnidga <oqomsm@gmail.com>
2024-08-03 19:54:02 +0300

    q

--- packages.nix
+++ packages.nix
@@ -1,66 +1,66 @@
 { pkgs, ... }: {
-  nixpkgs.config = {
-    allowUnfree = true;
-  };
+	nixpkgs.config = {
+		allowUnfree = true;
+	};
 
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    mesa-demos
-    git
-    xorg.xrandr
-    hwinfo
-    inxi
-    lshw
-    gh
-    xfce.thunar
-    discord
-    slstatus
-    fastfetch
-    kitty
-    neofetch
-    vlc
-    abiword
-    libreoffice
-    hunspellDicts.uk-ua
-    htop
-    fd
-    bat
-    nitrogen
-    ffmpeg
-    clamav
-    xclip
-    system-config-printer
-    pciutils
-    obs-studio
-    xkb-switch
-    zoom-us
-    cnijfilter_4_00
-    brave
-    dmenu
-    flameshot
-    pavucontrol
-    xorg.xinit
-    xorg.xorgserver
-    openssl
-    telegram-desktop
-  ];
+	environment.systemPackages = with pkgs; [
+		vim
+		wget
+		mesa-demos
+		git
+		xorg.xrandr
+		hwinfo
+		inxi
+		lshw
+		gh
+		xfce.thunar
+		discord
+		slstatus
+		fastfetch
+		kitty
+		neofetch
+		vlc
+		abiword
+		libreoffice
+		hunspellDicts.uk-ua
+		htop
+		fd
+		bat
+		nitrogen
+		ffmpeg
+		clamav
+		xclip
+		system-config-printer
+		pciutils
+		obs-studio
+		xkb-switch
+		zoom-us
+		cnijfilter_4_00
+		brave
+		dmenu
+		flameshot
+		pavucontrol
+		xorg.xinit
+		xorg.xorgserver
+		openssl
+		telegram-desktop
+	];
 
-  fonts.packages = with pkgs; [
-    jetbrains-mono
-    noto-fonts
-    noto-fonts-emoji
-    twemoji-color-font
-    font-awesome
-    powerline-fonts
-    powerline-symbols
-    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
-  ];
+	fonts.packages = with pkgs; [
+		jetbrains-mono
+		noto-fonts
+		noto-fonts-emoji
+		twemoji-color-font
+		font-awesome
+		powerline-fonts
+		powerline-symbols
+		(nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+	];
 
-  nixpkgs.overlays = [
-    (final: prev: { 
-      dwm = prev.dwm.overrideAttrs (_: { src = ./dwm; });
-      slstatus = prev.slstatus.overrideAttrs (_: { src = ./slstatus; });
-    })
-  ];
+	nixpkgs.overlays = [
+		(final: prev: { 
+			dwm = prev.dwm.overrideAttrs (_: { src = ./dwm; });
+			slstatus = prev.slstatus.overrideAttrs (_: { src = ./slstatus; });
+		})
+	];
 }
